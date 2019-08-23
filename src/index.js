203eaae9ace3b02572fe06bf190dd73967c95546
rkiv <rkiv@umich.edu>
2019-08-23 11:02:31 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const FPS = 30;
+
+/** @type {HTMLCanvasElement} */
+let canv = document.getElementById("gameCanvas");
+let ctx = canv.getContext("2d");
+
+// setup the game loop
+setInterval(update, 1000 / FPS);
+
+
+function update() {
+    // Draw space
+
+    // Draw Ship
+
+    // Rotate Ship
+
+    // Move Ship
+    
+}
