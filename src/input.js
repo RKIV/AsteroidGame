e1769c84ace34b0ef1c832a9271f0b15cdd74c13
rkiv <rkiv@umich.edu>
2019-08-24 16:58:33 -0400

    Add controls for thrust and turning

--- src/input.js
+++ src/input.js
@@ -1,21 +1,37 @@
 export default class InputHandler {
-     constructor() {
+     constructor(game) {
          document.addEventListener("keydown", event => {
             switch(event.keyCode){
                 // Left Arrow
                 case 37:
+                    game.ship.turnLeft();
                     break;
                 // Up Arrow
                 case 38:
+                    game.ship.thrusting = true;
                     break;
                 // Right Arrow
                 case 39:
+                    game.ship.turnRight();
                     break;
             }
          })
 
          document.addEventListener("keyup", event => {
-            
+            switch(event.keyCode){
+                // Left Arrow
+                case 37:
+                    game.ship.stopTurn();
+                    break;
+                // Up Arrow
+                case 38:
+                    game.ship.thrusting = false;
+                    break;
+                // Right Arrow
+                case 39:
+                    game.ship.stopTurn();
+                    break;
+            }
         })
      }
 }
--- src/ship.js
+++ src/ship.js
@@ -1,18 +1,39 @@
 export default class Ship {
     constructor(game) {
+        this.game = game;
         this.position = {
             x: game.width / 2,
             y: game.height / 2,
         };
-        this.angle = 0.1 * Math.PI;
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+        this.thrusting = false;
+        this.angle = 0.1 * Math.PI; // In Radians
+        this.angularSpeed = 0;
 
+    }
+
+    update() {
+        // Rotate Ship
+        this.angle += this.angularSpeed;
+        if(this.thrusting) {
+            this.velocity.x += Ship.THRUST * Math.cos(this.angle) / this.game.FPS;
+            this.velocity.y += Ship.THRUST * Math.sin(this.angle) / this.game.FPS;
+        }
+        this.velocity.x *= Ship.DECELERATION;
+        this.velocity.y *= Ship.DECELERATION;
+        this.position.x += this.velocity.x;
+        this.position.y -= this.velocity.y;
+        
     }
 
     draw (ctx) {
         ctx.strokeStyle = "white";
         ctx.lineWidth = Ship.RADIUS / 10;
         ctx.beginPath();
-        // nose of the ship
+        // Nose of the ship
         ctx.moveTo(
             this.position.x + (4 / 3) *Ship.RADIUS * Math.cos(this.angle),
             this.position.y - (4 / 3) * Ship.RADIUS * Math.sin(this.angle)
@@ -39,8 +60,17 @@ export default class Ship {
         ctx.fillStyle = "red";
         ctx.fillRect(this.position.x - 1, this.position.y - 1, 2, 2);
     }
+
+    turnLeft(){ this.angularSpeed = Ship.TURN_SPEED / this.game.FPS; }
+
+    turnRight(){ this.angularSpeed = -Ship.TURN_SPEED / this.game.FPS; }
+
+    stopTurn(){ this.angularSpeed = 0; }
 }
 
 
 // Ship Constant Static Variables
 Ship.RADIUS = 15;
+Ship.TURN_SPEED = 2 * Math.PI // radians/sec
+Ship.THRUST = 5; // Acceleration in pixels/sec^2
+Ship.DECELERATION = 0.99;
