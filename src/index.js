a25a67f6ed8367504ef274f87181b3c239a39095
rkiv <rkiv@umich.edu>
2019-08-24 16:58:59 -0400

    Implementing Index and refractoring draw call

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import Game from './game.js';
-const FPS = 30;
+import InputHandler from './input.js';
 
 /** @type {HTMLCanvasElement} */
 let canv = document.getElementById("gameCanvas");
 let ctx = canv.getContext("2d");
 
 let game = new Game(canv.width, canv.height);
+let input = new InputHandler(game);
 
 
 // setup the game loop
-setInterval(update, 1000 / FPS);
+setInterval(update, 1000 / game.FPS);
 
 
 function update() {
-    // Draw space
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, game.width, game.height);
-
-    // Draw Triangular Ship
-    game.ship.draw(ctx); 
+    game.update();
+    game.draw(ctx);
 
     // Rotate Ship
 
